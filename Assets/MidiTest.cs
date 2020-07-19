2d2bbc5611b6c1d756947915c87dd7437ff7a7c2
Petroochio <ptgyory@gmail.com>
2020-07-19 09:15:16 -0600

    put in correct size animation for github

--- Assets/MidiTest.cs
+++ Assets/MidiTest.cs
@@ -14,20 +14,14 @@ public class MidiTest : MonoBehaviour
     private IOutputDevice output;
     // Start is called before the first frame update
 
-    public double tempFreq = 440.0;
-    private double increment;
-    private double samplingFreq = 48000.0;
-    private float volume = 0.01f;
-    private double phase = 0.0;
-
-    private float gain = 0.0001f;
-    private float noteTime = 0.0f;
-
-    private float[] freq1 = new float[2048];
-
     // connect to drum pad here
     public Renderer drumPad;
     public Color drumColor = Color.red;
+
+    public AudioSource topLeft;
+    public AudioSource topRight;
+    public AudioSource lowLeft;
+    public AudioSource lowRight;
 
     public Animator animator;
     public bool shouldAnimate = false;
@@ -35,99 +29,47 @@ public class MidiTest : MonoBehaviour
 
     private AudioSource audioSource;
     public int position = 0;
-    public int samplerate = 44100;
-    public float frequency = 440;
-    public float playTime = 0.0f;
-    public float t;
 
     void Start()
     {
         animator = gameObject.GetComponent<Animator>();
-        increment = tempFreq * 2.0 * Mathf.PI / samplingFreq;
-
-        for (int i = 0; i < freq1.Length; i += 2) {
-            phase += increment;
-            freq1[i] = (float)(gain * Mathf.Sin((float)phase));
-            freq1[i + 1] = freq1[i];
-
-            if (phase >= (Mathf.PI * 2)) {
-                phase = 0.0;
-            }
-        }
 
         _inputDevice = InputDevice.GetById(0);
         _inputDevice.EventReceived += OnMIDIEventReceived;
         _inputDevice.StartEventsListening();
-
-        // var devicesConnector = _inputDevice.Connect(output);
-        
-        // foreach (var outputDevice in OutputDevice.GetAll())
-        // {
-        //     Console.WriteLine(outputDevice.Name);
-        // }
-        int sampleFreq = 44000;
-        float frequency = 440;
-        
-        float[] samples = new float[44000];
-        for(int i = 0; i < samples.Length; i++)
-        {
-            samples[i] = Mathf.Sin(Mathf.PI*2*i*frequency/sampleFreq);
-        }
-        AudioClip myClip = AudioClip.Create("MySinusoid", samplerate * 5, 1, samplerate, false);
-        audioSource = GetComponent<AudioSource>();
-        audioSource.clip = myClip;
-        // aud.Play();
-        myClip.SetData(samples, 0);
     }
 
     
     // Update is called once per frame
     void Update()
     {
-        noteTime -= Time.deltaTime;
         drumPad.material.SetColor("_Color", drumColor);
-        if (gain >= 0.001f) {
-            // gain -= 0.4f * Time.deltaTime;
-            // gain *= 0.95f;
-        }
 
-        t = Time.time;
         if (shouldAnimate) {
             shouldAnimate = false;
-            animator.SetTrigger(triggerName);
+            // animator.SetTrigger(triggerName);
             // trigger sound
-            audioSource.Play();
-            playTime = 0.1f;
-            Debug.Log(Time.time);
-        }
-
-        if (playTime > 0.0f) {
-            playTime -= Time.deltaTime;
-
-            if (playTime <= 0.0f) {
-                audioSource.Stop();
+            if (triggerName == "l_l_hit") {
+                lowLeft.Play();
+                animator.SetTrigger("l_hit");
+            }
+            if (triggerName == "l_r_hit") {
+                lowRight.Play();
+                animator.SetTrigger("r_hit");
+            }
+            if (triggerName == "u_l_hit") {
+                topLeft.Play();
+                animator.SetTrigger("l_hit");
+            }
+            if (triggerName == "u_r_hit") {
+                topRight.Play();
+                animator.SetTrigger("r_hit");
             }
         }
     }
 
     void OnApplicationQuit() {
         (_inputDevice as IDisposable)?.Dispose();
-    }
-
-    void OnAudioRead(float[] data)
-    {
-        int count = 0;
-        while (count < data.Length)
-        {
-            data[count] = Mathf.Sin(2 * Mathf.PI * frequency * position / samplerate);
-            position++;
-            count++;
-        }
-    }
-
-    void OnAudioSetPosition(int newPosition)
-    {
-        position = newPosition;
     }
 
     private void OnMIDIEventReceived(object sender, MidiEventReceivedEventArgs e)
@@ -137,34 +79,30 @@ public class MidiTest : MonoBehaviour
         if ((e.Event as NoteEvent).Velocity != 0) {
             // Debug.Log((e.Event as NoteEvent).Velocity);
             if ((e.Event as NoteEvent).NoteNumber == 2) {
-                tempFreq = 540.0;
                 drumColor = Color.red;
                 shouldAnimate = true;
-                triggerName = "l_hit";
+                triggerName = "u_l_hit";
             }
             if ((e.Event as NoteEvent).NoteNumber == 95) {
                 // Right high
-                tempFreq = 440.0;
+                // tempFreq = 440.0;
                 drumColor = Color.blue;
                 shouldAnimate = true;
-                triggerName = "r_hit";
+                triggerName = "u_r_hit";
             }
             if ((e.Event as NoteEvent).NoteNumber == 3) {
-                tempFreq = 340.0;
+                // tempFreq = 340.0;
                 drumColor = Color.green;
                 shouldAnimate = true;
-                triggerName = "l_hit";
+                triggerName = "l_l_hit";
             }
             if ((e.Event as NoteEvent).NoteNumber == 4) {
                 // Right low
-                tempFreq = 240.0;
+                // tempFreq = 240.0;
                 drumColor = Color.yellow;
                 shouldAnimate = true;
-                triggerName = "r_hit";
+                triggerName = "l_r_hit";
             }
-            gain = 0.1f;
-            noteTime = 0.001f * (e.Event as NoteEvent).Velocity;
-            // Debug.Log((e.Event as NoteEvent).NoteNumber);
         } else {
             // gain = 0.0f;
         }
