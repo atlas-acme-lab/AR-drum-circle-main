b2bb4aaf51629de0f80128f945fa024b70559b14
Petroochio <ptgyory@gmail.com>
2020-09-29 09:22:32 -0600

    local client udp connection

---
+++ Assets/Scripts/Networking/UDPLocalMIDIConnector.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UDPLocalMIDIConnector : MonoBehaviour
+{
+    public Text myText;
+    public MidiTest drum;
+    private AndroidJavaObject udpLocalClass;
+    public string ip = "127.0.0.1";
+    // Start is called before the first frame update
+    void Start()
+    {
+        udpLocalClass = new AndroidJavaObject("com.acme.networkinglibrary.UDPMidiConnector");
+        udpLocalClass.Call("StartConnectionThread", ip);
+    }
+
+    public void PlayNoteFromLocal(string midiInfo) {
+        Debug.Log("got msg from native " + midiInfo);
+        string[] note = midiInfo.Split(',');
+        drum.PlayMIDINote(Convert.ToInt32(note[1]), 10);
+    }
+}
