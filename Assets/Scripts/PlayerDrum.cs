df4a37fe2bb002ed00737f600148ee57b0481dde
Petroochio <ptgyory@gmail.com>
2020-10-08 16:55:39 -0600

    update drum and particle colors

--- Assets/Scripts/PlayerDrum.cs
+++ Assets/Scripts/PlayerDrum.cs
@@ -8,7 +8,6 @@ public class PlayerDrum : MonoBehaviour
     public GameObject audioPrefab;
 
     public Renderer drumPad;
-    public Color drumColor = Color.red;
 
     public ParticleSystem hitParticles;
 
@@ -16,13 +15,23 @@ public class PlayerDrum : MonoBehaviour
     public bool shouldAnimate = false;
     public string triggerName = "l_hit";
 
+    private List<Color> palette = new List<Color>();
+    public ParticleSystem particles;
+
+    public Color drumColor;
+
     private float velocity;
     public float maxAnimSpeed = 3f;
     public float minAnimSpeed = 1f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        palette.Add(new Color(0.9f, 0.31f, 0.42f));
+        palette.Add(new Color(0.9f, 0.55f, 0.21f));
+        palette.Add(new Color(0.33f, 0.42f, 0.78f));
+        palette.Add(new Color(0.47f, 0.59f, 0.93f));
+
+        drumColor = palette[0];
     }
 
     public void PlayMIDINote(int note, int velocity) {
@@ -36,7 +45,7 @@ public class PlayerDrum : MonoBehaviour
         {
             if (note == 1)
             {
-                drumColor = Color.red;
+                drumColor = palette[0];
                 shouldAnimate = true;
                 triggerName = "u_l_hit";
             }
@@ -44,14 +53,14 @@ public class PlayerDrum : MonoBehaviour
             {
                 // Right high
                 // tempFreq = 440.0;
-                drumColor = Color.blue;
+                drumColor = palette[1];
                 shouldAnimate = true;
                 triggerName = "u_r_hit";
             }
             if (note == 2)
             {
                 // tempFreq = 340.0;
-                drumColor = Color.green;
+                drumColor = palette[2];
                 shouldAnimate = true;
                 triggerName = "l_l_hit";
             }
@@ -59,7 +68,7 @@ public class PlayerDrum : MonoBehaviour
             {
                 // Right low
                 // tempFreq = 240.0;
-                drumColor = Color.yellow;
+                drumColor = palette[3];
                 shouldAnimate = true;
                 triggerName = "l_r_hit";
             }
@@ -70,6 +79,7 @@ public class PlayerDrum : MonoBehaviour
     void Update()
     {
         drumPad.material.SetColor("_Color", drumColor);
+        particles.startColor = drumColor;
 
         if (shouldAnimate)
         {
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject logo;
+    public GameObject button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void hideUI() {
+        
+    }
+}
